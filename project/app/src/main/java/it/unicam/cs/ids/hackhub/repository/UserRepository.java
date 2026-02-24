de616319e675cba5e898cb7a44d8d1c955115bcc
Redcoral-17 <filippo.corallini@studenti.unicam.it>
2026-02-24 17:20:59 +0100

    Implement UserRepository to initialize with an empty list of users

--- project/app/src/main/java/it/unicam/cs/ids/hackhub/repository/UserRepository.java
+++ project/app/src/main/java/it/unicam/cs/ids/hackhub/repository/UserRepository.java
@@ -1,5 +1,15 @@
 package it.unicam.cs.ids.hackhub.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserRepository implements Repository<User> {
+
+    private List<User> users;
+
+    public UserRepository() {
+        users = new ArrayList<>();
+    }
+
 
 }
