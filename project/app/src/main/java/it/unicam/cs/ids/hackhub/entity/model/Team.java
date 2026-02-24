ed9be8985940edbc2c7ce08eeda19385be07118c
MtoSmi <matteo.simonetti@studenti.unicam.it>
2026-02-24 17:10:27 +0100

    Add properties, constructor, and accessors to Team entity model class

--- project/app/src/main/java/it/unicam/cs/ids/hackhub/entity/model/Team.java
+++ project/app/src/main/java/it/unicam/cs/ids/hackhub/entity/model/Team.java
@@ -5,7 +5,63 @@ import java.util.List;
 public class Team {
     private Long id;
     private String name;
-    private int dimension = 0;
+    private int dimension;
     private List<User> members;
     private List<Hackathon> hackathons;
+
+    public Team(String name) {
+        this.name = name;
+        this.dimension = 1;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDimension() {
+        return dimension;
+    }
+
+    public void setDimension(int dimension) {
+        this.dimension = dimension;
+    }
+
+    public List<User> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<User> members) {
+        this.members = members;
+    }
+
+    public List<Hackathon> getHackathons() {
+        return hackathons;
+    }
+
+    public void setHackathons(List<Hackathon> hackathons) {
+        this.hackathons = hackathons;
+    }
+
+    @Override
+    public String toString() {
+        return "Team{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", dimension=" + dimension +
+                ", members=" + members +
+                ", hackathons=" + hackathons +
+                '}';
+    }
 }
