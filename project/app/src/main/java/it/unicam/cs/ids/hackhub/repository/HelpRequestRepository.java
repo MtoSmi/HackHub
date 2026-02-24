6f010971c4865cdff5b8bbc798a47b87107f3673
Redcoral-17 <filippo.corallini@studenti.unicam.it>
2026-02-24 16:46:47 +0100

    Add constructor to HelpRequestRepository for initializing help requests

--- project/app/src/main/java/it/unicam/cs/ids/hackhub/repository/HelpRequestRepository.java
+++ project/app/src/main/java/it/unicam/cs/ids/hackhub/repository/HelpRequestRepository.java
@@ -2,4 +2,11 @@ package it.unicam.cs.ids.hackhub.repository;
 
 public class HelpRequestRepository extends Repository<HelpRequest> {
 
+    private List<HelpRequest> helpRequests;
+
+    public HelpRequestRepository(List<HelpRequest> helpRequests) {
+        this.helpRequests = helpRequests;
+    }
+
+
 }
