52d5e4cb51e89c0d9795838e77e56c7e80b7c719
MtoSmi <matteo.simonetti@studenti.unicam.it>
2026-02-24 16:37:32 +0100

    Add properties to HelpRequest entity model class

--- project/app/src/main/java/it/unicam/cs/ids/hackhub/entity/model/HelpRequest.java
+++ project/app/src/main/java/it/unicam/cs/ids/hackhub/entity/model/HelpRequest.java
@@ -1,4 +1,12 @@
 package it.unicam.cs.ids.hackhub.entity.model;
 
 public class HelpRequest {
+    private Long id;
+    private String title;
+    private String description;
+    private String reply;
+    private User from;
+    private User to;
+    private String call;
+    private boolean completed;
 }
