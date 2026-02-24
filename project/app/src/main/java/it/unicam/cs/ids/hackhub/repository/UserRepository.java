4dfdf1710dcd124056a861e24c4467a22ec463a8
Redcoral-17 <filippo.corallini@studenti.unicam.it>
2026-02-24 17:22:30 +0100

    Implement create method in UserRepository to add a new user

--- project/app/src/main/java/it/unicam/cs/ids/hackhub/repository/UserRepository.java
+++ project/app/src/main/java/it/unicam/cs/ids/hackhub/repository/UserRepository.java
@@ -16,4 +16,9 @@ public class UserRepository implements Repository<User> {
         return users;
     }
 
+    @Override
+    public void create(User u) {
+        setId(1L);
+        users.add(u);
+    }
 }
