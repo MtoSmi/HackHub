0ae3deeec4d4e8af17a8b4921a954bdc97072fd8
MtoSmi <matteo.simonetti@studenti.unicam.it>
2026-02-24 16:27:36 +0100

    Add properties to User entity model class

--- project/app/src/main/java/it/unicam/cs/ids/hackhub/entity/model/User.java
+++ project/app/src/main/java/it/unicam/cs/ids/hackhub/entity/model/User.java
@@ -1,4 +1,16 @@
 package it.unicam.cs.ids.hackhub.entity.model;
 
+import it.unicam.cs.ids.hackhub.entity.enumeration.Rank;
+
+/**
+ * Rappresenta un utente del sistema HackHub.
+ */
 public class User {
+    private Long id;
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+    private Team team = null;
+    private Rank rank;
 }
