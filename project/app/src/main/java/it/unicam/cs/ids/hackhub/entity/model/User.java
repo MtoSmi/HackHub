7ea8a51fda2cf1f4eee4870004fccde70c096e77
MtoSmi <matteo.simonetti@studenti.unicam.it>
2026-02-24 16:49:32 +0100

    Add constructor and accessors to User entity model class

--- project/app/src/main/java/it/unicam/cs/ids/hackhub/entity/model/User.java
+++ project/app/src/main/java/it/unicam/cs/ids/hackhub/entity/model/User.java
@@ -11,6 +11,84 @@ public class User {
     private String surname;
     private String email;
     private String password;
-    private Team team = null;
+    private Team team;
     private Rank rank;
+
+    public User(String name, String surname, String email, String password) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.password = password;
+        this.team = null;
+        this.rank = Rank.STANDARD;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
+    public void setRank(Rank rank) {
+        this.rank = rank;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", team=" + team +
+                ", rank=" + rank +
+                '}';
+    }
 }
