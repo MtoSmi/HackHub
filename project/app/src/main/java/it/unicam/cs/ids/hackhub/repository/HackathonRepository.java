7024ec5ab5d1e9bf4d2e9f04972953005cd31f6f
Redcoral-17 <filippo.corallini@studenti.unicam.it>
2026-02-24 16:34:00 +0100

    Implement getById method in HackathonRepository for retrieving hackathons by ID

--- project/app/src/main/java/it/unicam/cs/ids/hackhub/repository/HackathonRepository.java
+++ project/app/src/main/java/it/unicam/cs/ids/hackhub/repository/HackathonRepository.java
@@ -7,8 +7,17 @@ public class HackathonRepository extends Repository<Hackathon> {
     public HackathonRepository(List<Hackathon> hackathons) {
         this.hackathons = hackathons;
     }
+
     @Override
     public List<Hackathon> getAll() {
         return this.hackathons;
+    }
+
+    @Override
+    public Hackathon getById(Long id) {
+        for(Hackathon h : hackathons){
+            if(h.getId().equals(id)) return h;
+        }
+        return null;
     }
 }
