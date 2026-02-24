602c976c7d51952d849159dda907e4ff2978fe1b
Redcoral-17 <filippo.corallini@studenti.unicam.it>
2026-02-24 17:14:57 +0100

    Implement TeamRepository to initialize with an empty list of teams

--- project/app/src/main/java/it/unicam/cs/ids/hackhub/repository/TeamRepository.java
+++ project/app/src/main/java/it/unicam/cs/ids/hackhub/repository/TeamRepository.java
@@ -1,6 +1,14 @@
 package it.unicam.cs.ids.hackhub.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TeamRepository implements Repository<Team> {
 
+    private List<Team> teams;
+
+    public TeamRepository() {
+        teams = new ArrayList<>();
+    }
 
 }
