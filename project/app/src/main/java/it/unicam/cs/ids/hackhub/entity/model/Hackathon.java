306d8d9a377c8c871346fa4fa79737a75bff81e7
MtoSmi <matteo.simonetti@studenti.unicam.it>
2026-02-24 16:35:17 +0100

    Add Hackathon and Submission entity model classes

--- project/app/src/main/java/it/unicam/cs/ids/hackhub/entity/model/Hackathon.java
+++ project/app/src/main/java/it/unicam/cs/ids/hackhub/entity/model/Hackathon.java
@@ -1,4 +1,23 @@
 package it.unicam.cs.ids.hackhub.entity.model;
 
+import it.unicam.cs.ids.hackhub.entity.enumeration.Status;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class Hackathon {
+    private Long id;
+    private String name;
+    private User host;
+    private User judge;
+    private List<User> mentors;
+    private List<Team> participants;
+    private int maxTeams;
+    private List<Submission> submissions;
+    private String regulation;
+    private LocalDateTime deadline;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private String location;
+    private Status status;
 }
