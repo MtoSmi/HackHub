468e9dd0f0ccd0de7afd12111468bd715e4630c1
Redcoral-17 <filippo.corallini@studenti.unicam.it>
2026-02-24 16:31:21 +0100

    Refactor HackathonRepository to extend Repository and implement getAll method

--- project/app/src/main/java/it/unicam/cs/ids/hackhub/repository/HackathonRepository.java
+++ project/app/src/main/java/it/unicam/cs/ids/hackhub/repository/HackathonRepository.java
@@ -1,4 +1,14 @@
 package it.unicam.cs.ids.hackhub.repository;
 
-public class HackathonRepository {
+public class HackathonRepository extends Repository<Hackathon> {
+
+    private List<Hackathon> hackathons;
+
+    public HackathonRepository(List<Hackathon> hackathons) {
+        this.hackathons = hackathons;
+    }
+    @Override
+    public List<Hackathon> getAll() {
+        return this.hackathons;
+    }
 }
