71acea29b5f8686718bb9a6a37b3467c2739c724
MtoSmi <matteo.simonetti@studenti.unicam.it>
2026-02-24 16:29:47 +0100

    Add properties to Team entity model class

--- project/app/src/main/java/it/unicam/cs/ids/hackhub/entity/model/Team.java
+++ project/app/src/main/java/it/unicam/cs/ids/hackhub/entity/model/Team.java
@@ -1,4 +1,11 @@
 package it.unicam.cs.ids.hackhub.entity.model;
 
+import java.util.List;
+
 public class Team {
+    private Long id;
+    private String name;
+    private int dimension = 0;
+    private List<User> members;
+    private List<Hackathon> hackathons;
 }
