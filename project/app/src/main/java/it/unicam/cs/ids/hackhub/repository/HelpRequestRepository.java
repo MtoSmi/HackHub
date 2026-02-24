d01869c94c87a362618c23a83b2687dde0603e2d
Redcoral-17 <filippo.corallini@studenti.unicam.it>
2026-02-24 16:58:20 +0100

    Refactor HelpRequestRepository to initialize with an empty list and implement getById method for retrieving help requests by ID

--- project/app/src/main/java/it/unicam/cs/ids/hackhub/repository/HelpRequestRepository.java
+++ project/app/src/main/java/it/unicam/cs/ids/hackhub/repository/HelpRequestRepository.java
@@ -1,16 +1,27 @@
 package it.unicam.cs.ids.hackhub.repository;
 
-public class HelpRequestRepository extends Repository<HelpRequest> {
+import java.util.ArrayList;
+import java.util.List;
+
+public class HelpRequestRepository implements Repository<HelpRequest> {
 
     private List<HelpRequest> helpRequests;
 
-    public HelpRequestRepository(List<HelpRequest> helpRequests) {
-        this.helpRequests = helpRequests;
+    public HelpRequestRepository() {
+        this.helpRequests = new ArrayList<>();
     }
 
     @Override
     public List<HelpRequest> getByMentor(Long id) {
         return this.helpRequests.stream().filter(hr -> hr.getMentor().getId().equals(id)).toList();
+    }
+
+    @Override
+    public HelpRequest getById(Long id) {
+        for(HelpRequest hr : helpRequests){
+            if(hr.getId().equals(id)) return hr;
+        }
+        return null;
     }
 
 }
