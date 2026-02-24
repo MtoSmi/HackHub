2d741c00722b30eed47c103d3627a5ad37f06469
MtoSmi <matteo.simonetti@studenti.unicam.it>
2026-02-24 16:38:15 +0100

    Add properties to Notification entity model class

--- project/app/src/main/java/it/unicam/cs/ids/hackhub/entity/model/Notification.java
+++ project/app/src/main/java/it/unicam/cs/ids/hackhub/entity/model/Notification.java
@@ -1,4 +1,8 @@
 package it.unicam.cs.ids.hackhub.entity.model;
 
 public class Notification {
+    private Long id;
+    private String title;
+    private String description;
+    private User to;
 }
