a51dd8ebd58cfad87ef6683920284fbf540dc90b
Redcoral-17 <filippo.corallini@studenti.unicam.it>
2026-02-24 16:17:04 +0100

    Enhance Repository interface with generic type and CRUD methods

--- project/app/src/main/java/it/unicam/cs/ids/hackhub/repository/Repository.java
+++ project/app/src/main/java/it/unicam/cs/ids/hackhub/repository/Repository.java
@@ -1,4 +1,10 @@
 package it.unicam.cs.ids.hackhub.repository;
 
-public interface Repository {
+import java.util.List;
+
+public interface Repository<T> {
+    List<T> getAll();
+    T getById(Long Id);
+    void create(T entity);
+    void update(T entity);
 }
