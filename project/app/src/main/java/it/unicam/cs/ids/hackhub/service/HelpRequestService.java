037bc3f46f38b10f6cd515a30290ab27e4d6da67
Redcoral-17 <filippo.corallini@studenti.unicam.it>
2026-02-25 13:45:55 +0100

    Add method to HelpRequestService for retrieving help requests by mentor

--- project/app/src/main/java/it/unicam/cs/ids/hackhub/service/HelpRequestService.java
+++ project/app/src/main/java/it/unicam/cs/ids/hackhub/service/HelpRequestService.java
@@ -1,6 +1,9 @@
 package it.unicam.cs.ids.hackhub.service;
 
+import it.unicam.cs.ids.hackhub.entity.model.HelpRequest;
 import it.unicam.cs.ids.hackhub.repository.HelpRequestRepository;
+
+import java.util.List;
 
 public class HelpRequestService {
 
@@ -11,4 +14,9 @@ public class HelpRequestService {
         this.helpRequestRepository = hRepo;
         this.notificationService = nService;
     }
+
+    public List<HelpRequest> showMyHelpRequests(Long mentorId) {
+        return helpRequestRepository.getByMentor(mentorId);
+    }
+
 }
