c7896ddc545d073585b7236b59ca1ea0bbb59901
Redcoral-17 <filippo.corallini@studenti.unicam.it>
2026-02-25 13:41:42 +0100

    Add notification functionality to HackathonService for judges and mentors

--- project/app/src/main/java/it/unicam/cs/ids/hackhub/service/HackathonService.java
+++ project/app/src/main/java/it/unicam/cs/ids/hackhub/service/HackathonService.java
@@ -2,6 +2,7 @@ package it.unicam.cs.ids.hackhub.service;
 
 import it.unicam.cs.ids.hackhub.builder.HackathonConcreteBuilder;
 import it.unicam.cs.ids.hackhub.entity.model.Hackathon;
+import it.unicam.cs.ids.hackhub.entity.model.User;
 import it.unicam.cs.ids.hackhub.repository.HackathonRepository;
 import it.unicam.cs.ids.hackhub.validator.HackathonValidator;
 
@@ -10,10 +11,12 @@ import java.util.List;
 public class HackathonService {
     private final HackathonRepository hackathonRepository;
     private final HackathonValidator hackathonValidator;
+    private final NotificationService notificationService;
 
-    public HackathonService(HackathonRepository hRepo, HackathonValidator hValid) {
+    public HackathonService(HackathonRepository hRepo, HackathonValidator hValid, NotificationService nService) {
         this.hackathonRepository = hRepo;
         this.hackathonValidator = hValid;
+        this.notificationService = nService;
     }
 
     public List<Hackathon> showHackathonList() {
@@ -32,5 +35,13 @@ public class HackathonService {
                 .buildRegulation(h.getRegulation()).buildDeadline(h.getDeadline())
                 .buildStartDate(h.getStartDate()).buildEndDate(h.getEndDate())
                 .buildLocation(h.getLocation()).buildReward(h.getReward()).getResult());
+        notificationService.send("Sei un giudice!",
+                "Sei appena diventato un giudice del nuovo hackathon " + h.getName(),
+                h.getJudge().getId());
+        for(User mentor : h.getMentors()) {
+            notificationService.send("Sei un mentore!",
+                    "Sei appena diventato un mentore del nuovo hackathon " + h.getName(),
+                    mentor.getId());
+        }
     }
 }
