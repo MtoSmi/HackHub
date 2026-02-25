38a24c3d98474f1dfd1975849ebcd962204cc888
Redcoral-17 <filippo.corallini@studenti.unicam.it>
2026-02-25 11:01:50 +0100

    Add constructor to HackathonService for repository and validator injection

--- project/app/src/main/java/it/unicam/cs/ids/hackhub/service/HackathonService.java
+++ project/app/src/main/java/it/unicam/cs/ids/hackhub/service/HackathonService.java
@@ -1,4 +1,16 @@
 package it.unicam.cs.ids.hackhub.service;
 
+import it.unicam.cs.ids.hackhub.repository.HackathonRepository;
+import it.unicam.cs.ids.hackhub.validator.HackathonValidator;
+
 public class HackathonService {
+    private HackathonRepository hackathonRepository;
+    private HackathonValidator hackathonValidator;
+
+    public HackathonService(HackathonRepository hRepo, HackathonValidator hValid) {
+        this.hackathonRepository = hRepo;
+        this.hackathonValidator = hValid;
+    }
+
+
 }
