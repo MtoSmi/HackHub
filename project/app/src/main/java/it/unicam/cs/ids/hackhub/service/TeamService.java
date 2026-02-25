582d4c4ce31025b58997405da3f023884b206003
Redcoral-17 <filippo.corallini@studenti.unicam.it>
2026-02-25 14:02:24 +0100

    Add TeamValidator to TeamService for team creation validation

--- project/app/src/main/java/it/unicam/cs/ids/hackhub/service/TeamService.java
+++ project/app/src/main/java/it/unicam/cs/ids/hackhub/service/TeamService.java
@@ -1,11 +1,20 @@
 package it.unicam.cs.ids.hackhub.service;
 
+import it.unicam.cs.ids.hackhub.entity.model.Team;
 import it.unicam.cs.ids.hackhub.repository.TeamRepository;
+import it.unicam.cs.ids.hackhub.validator.TeamValidator;
 
 public class TeamService {
     private final TeamRepository teamRepository;
+    private final TeamValidator teamValidator;
 
-    public TeamService(TeamRepository tRepo) {
+    public TeamService(TeamRepository tRepo, TeamValidator tValid) {
         this.teamRepository = tRepo;
+        this.teamValidator = tValid;
+    }
+
+    public void creationTeam(Team t) {
+        if(!teamValidator.validate(t)) return;
+        teamRepository.create(t);
     }
 }
