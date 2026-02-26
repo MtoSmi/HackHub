753ed32f1ddedebdd99ce1e21c68924feec8dc0a
MtoSmi <matteo.simonetti@studenti.unicam.it>
2026-02-26 10:48:11 +0100

    Implement UserInterfaceController with user registration functionality

--- project/app/src/main/java/it/unicam/cs/ids/hackhub/controller/UserInterfaceController.java
+++ project/app/src/main/java/it/unicam/cs/ids/hackhub/controller/UserInterfaceController.java
@@ -1,4 +1,17 @@
 package it.unicam.cs.ids.hackhub.controller;
 
+import it.unicam.cs.ids.hackhub.entity.model.User;
+import it.unicam.cs.ids.hackhub.entity.requester.UserRequester;
+import it.unicam.cs.ids.hackhub.service.UserService;
+
 public class UserInterfaceController {
+    private final UserService service;
+
+    public UserInterfaceController(UserService service){
+        this.service = service;
+    }
+
+    public User registrationUser(UserRequester requested) {
+        return service.registrationUser(requested);
+    }
 }
