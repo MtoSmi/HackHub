cb05b6aa8025dcea9c75d0eb458d052a39743926
MtoSmi <matteo.simonetti@studenti.unicam.it>
2026-02-26 10:07:29 +0100

    Implement HackathonInterfaceController with methods for creating and retrieving hackathons

--- project/app/src/main/java/it/unicam/cs/ids/hackhub/controller/HackathonInterfaceController.java
+++ project/app/src/main/java/it/unicam/cs/ids/hackhub/controller/HackathonInterfaceController.java
@@ -1,4 +1,27 @@
 package it.unicam.cs.ids.hackhub.controller;
 
+import it.unicam.cs.ids.hackhub.entity.model.Hackathon;
+import it.unicam.cs.ids.hackhub.entity.requester.HackathonRequester;
+import it.unicam.cs.ids.hackhub.service.HackathonService;
+
+import java.util.List;
+
 public class HackathonInterfaceController {
+    private final HackathonService service;
+
+    public HackathonInterfaceController(HackathonService service) {
+        this.service = service;
+    }
+
+    public Hackathon creationHackathon(HackathonRequester requested) {
+        return service.creationHackathon(requested);
+    }
+
+    public List<Hackathon> showHackathonList() {
+        return service.showHackathonList();
+    }
+
+    public Hackathon showSelectedHackathon(long id) {
+        return service.showSelectedHackathon(id);
+    }
 }
