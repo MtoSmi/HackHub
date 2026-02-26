91bf5037f5b9088fd7e0230f9c11d2cebf22e812
MtoSmi <matteo.simonetti@studenti.unicam.it>
2026-02-26 10:40:48 +0100

    Implement TeamInterfaceController with team creation functionality

--- project/app/src/main/java/it/unicam/cs/ids/hackhub/controller/TeamInterfaceController.java
+++ project/app/src/main/java/it/unicam/cs/ids/hackhub/controller/TeamInterfaceController.java
@@ -1,4 +1,17 @@
 package it.unicam.cs.ids.hackhub.controller;
 
+import it.unicam.cs.ids.hackhub.entity.model.Team;
+import it.unicam.cs.ids.hackhub.entity.requester.TeamRequester;
+import it.unicam.cs.ids.hackhub.service.TeamService;
+
 public class TeamInterfaceController {
+    private final TeamService service;
+
+    public TeamInterfaceController(TeamService service) {
+        this.service = service;
+    }
+
+    public Team creationTeam(TeamRequester requested) {
+        return service.creationTeam(requested);
+    }
 }
