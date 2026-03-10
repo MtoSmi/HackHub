31e231213ff34b8f468c0f24617f42e6c85ff83d
MtoSmi <matteo.simonetti@studenti.unicam.it>
2026-03-10 10:15:10 +0100

    Add MentorInterfaceController with mentor addition functionality

--- project/app/src/main/java/it/unicam/cs/ids/hackhub/controller/MentorInterfaceController.java
+++ project/app/src/main/java/it/unicam/cs/ids/hackhub/controller/MentorInterfaceController.java
@@ -1,4 +1,15 @@
 package it.unicam.cs.ids.hackhub.controller;
 
+import it.unicam.cs.ids.hackhub.entity.model.User;
+
 public class MentorInterfaceController {
+    private final MentorService service;
+
+    public MentorInterfaceController(MentorService service) {
+        this.service = service;
+    }
+
+    public void addMentor(User user, Long hackathonId) {
+        service.addMentor(user, hackathonId);
+    }
 }
