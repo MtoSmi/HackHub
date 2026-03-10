1af9af2a22c930a85e2172d506c2b0e12a381cf0
MtoSmi <matteo.simonetti@studenti.unicam.it>
2026-03-10 10:20:31 +0100

    Add NotificationInterfaceController with notification retrieval functionality

--- project/app/src/main/java/it/unicam/cs/ids/hackhub/controller/NotificationInterfaceController.java
+++ project/app/src/main/java/it/unicam/cs/ids/hackhub/controller/NotificationInterfaceController.java
@@ -1,4 +1,22 @@
 package it.unicam.cs.ids.hackhub.controller;
 
+import it.unicam.cs.ids.hackhub.entity.model.Notification;
+import it.unicam.cs.ids.hackhub.service.NotificationService;
+
+import java.util.List;
+
 public class NotificationInterfaceController {
+    private final NotificationService service;
+
+    public NotificationInterfaceController(NotificationService service) {
+        this.service = service;
+    }
+
+    public List<Notification> showMyNotifications(long userId) {
+        return service.showMyNotifications(userId);
+    }
+
+    public Notification showSelectedNotification(long id) {
+        return service.showSelectedNotification(id);
+    }
 }
