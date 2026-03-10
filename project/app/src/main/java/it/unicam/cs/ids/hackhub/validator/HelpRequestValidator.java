76fcdac620680c540cf10180af7a72b4548ca107
Redcoral-17 <filippo.corallini@studenti.unicam.it>
2026-03-10 09:29:52 +0100

    Add HelpRequestValidator class for validating help requests

---
+++ project/app/src/main/java/it/unicam/cs/ids/hackhub/validator/HelpRequestValidator.java
@@ -0,0 +1,7 @@
+package it.unicam.cs.ids.hackhub.validator;
+
+import it.unicam.cs.ids.hackhub.entity.model.HelpRequest;
+
+public class HelpRequestValidator implements Validator<HelpRequest> {
+
+}
