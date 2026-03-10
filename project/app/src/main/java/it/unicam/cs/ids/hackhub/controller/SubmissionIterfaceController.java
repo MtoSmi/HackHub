227a48f6fb1e54587e583e105ce455880050e871
MtoSmi <matteo.simonetti@studenti.unicam.it>
2026-03-10 10:03:45 +0100

    Add SubmissionIterfaceController with submission creation functionality

--- project/app/src/main/java/it/unicam/cs/ids/hackhub/controller/SubmissionIterfaceController.java
+++ project/app/src/main/java/it/unicam/cs/ids/hackhub/controller/SubmissionIterfaceController.java
@@ -1,4 +1,16 @@
 package it.unicam.cs.ids.hackhub.controller;
 
+import it.unicam.cs.ids.hackhub.entity.model.Submission;
+import it.unicam.cs.ids.hackhub.entity.requester.SubmissionRequester;
+
 public class SubmissionIterfaceController {
+    private final SubmissionService service;
+
+    public SubmissionIterfaceController(SubmissionService service) {
+        this.service = service;
+    }
+
+    public Submission creationSubmission(SubmissionRequester requested) {
+        return service.creationSubmission(requested);
+    }
 }
