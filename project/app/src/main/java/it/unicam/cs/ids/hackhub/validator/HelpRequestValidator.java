4fb890f4995338f99d07e8da5e3af4ff91dc59e1
Redcoral-17 <filippo.corallini@studenti.unicam.it>
2026-03-10 09:40:31 +0100

    Implement validation logic in HelpRequestValidator class

--- project/app/src/main/java/it/unicam/cs/ids/hackhub/validator/HelpRequestValidator.java
+++ project/app/src/main/java/it/unicam/cs/ids/hackhub/validator/HelpRequestValidator.java
@@ -4,4 +4,13 @@ import it.unicam.cs.ids.hackhub.entity.model.HelpRequest;
 
 public class HelpRequestValidator implements Validator<HelpRequest> {
 
+    @Override
+    public boolean validate(HelpRequest hr) {
+        if(hr == null) return false;
+        if(hr.getTitle() == null || hr.getTitle().isBlank()) return false;
+        if(hr.getDescription() == null || hr.getDescription().isBlank()) return false;
+        if(hr.getReply() != null && !hr.getReply().isBlank()) return false;
+        if(hr.getFrom() == null || hr.getTo() == null) return false;
+        return hr.getCall() != null && !hr.getCall().isBlank();
+    }
 }
