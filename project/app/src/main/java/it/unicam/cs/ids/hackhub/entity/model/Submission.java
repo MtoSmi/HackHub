396dfe14b2bfbd543a2d18bca5cd5a8aa906c31d
MtoSmi <matteo.simonetti@studenti.unicam.it>
2026-03-10 09:26:38 +0100

    Add fields to Submission class for enhanced data management

--- project/app/src/main/java/it/unicam/cs/ids/hackhub/entity/model/Submission.java
+++ project/app/src/main/java/it/unicam/cs/ids/hackhub/entity/model/Submission.java
@@ -1,5 +1,13 @@
 package it.unicam.cs.ids.hackhub.entity.model;
 
+import java.time.LocalDateTime;
+
 public class Submission {
-    //TODO sviluppo nella prossima iterazione
+    private long id;
+    private String title;
+    private String description;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private List<Response> responses;
+    private boolean complete;
 }
