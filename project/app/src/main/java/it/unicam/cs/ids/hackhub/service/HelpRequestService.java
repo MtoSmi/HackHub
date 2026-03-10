808a47b3733ab1a08d2c68a8fcaa99af1136a4e8
Redcoral-17 <filippo.corallini@studenti.unicam.it>
2026-03-10 10:43:08 +0100

    Add creationHelpRequest method to HelpRequestService for handling help request creation

--- project/app/src/main/java/it/unicam/cs/ids/hackhub/service/HelpRequestService.java
+++ project/app/src/main/java/it/unicam/cs/ids/hackhub/service/HelpRequestService.java
@@ -1,18 +1,26 @@
 package it.unicam.cs.ids.hackhub.service;
 
+import it.unicam.cs.ids.hackhub.entity.enumeration.Rank;
+import it.unicam.cs.ids.hackhub.entity.enumeration.Status;
+import it.unicam.cs.ids.hackhub.entity.model.Hackathon;
 import it.unicam.cs.ids.hackhub.entity.model.HelpRequest;
 import it.unicam.cs.ids.hackhub.entity.requester.HelpRequestRequester;
+import it.unicam.cs.ids.hackhub.repository.HackathonRepository;
 import it.unicam.cs.ids.hackhub.repository.HelpRequestRepository;
+import it.unicam.cs.ids.hackhub.validator.HelpRequestValidator;
 
 import java.util.List;
 
 public class HelpRequestService {
-
     private final HelpRequestRepository helpRequestRepository;
+    private final HelpRequestValidator helpRequestValidator;
+    private final HackathonRepository hackathonRepository;
     private final NotificationService notificationService;
 
-    public HelpRequestService(HelpRequestRepository hrRepo, NotificationService nService) {
+    public HelpRequestService(HelpRequestRepository hrRepo, HelpRequestValidator hrValidator, HackathonRepository hRepo, NotificationService nService) {
         this.helpRequestRepository = hrRepo;
+        this.helpRequestValidator = hrValidator;
+        this.hackathonRepository = hRepo;
         this.notificationService = nService;
     }
 
@@ -22,6 +30,21 @@ public class HelpRequestService {
 
     public HelpRequest showSelectedHelpRequest(Long id) {
         return helpRequestRepository.getById(id);
+    }
+
+    public HelpRequest creationHelpRequest(HelpRequestRequester hr) {
+        if(!helpRequestValidator.validate(hr)) return null;
+        if(!hr.getFrom().getRank().equals(Rank.MEMBRO_TEAM)) return null;
+        if(!hr.getTo().getRank().equals(Rank.MENTORE)) return null;
+        for(Hackathon h : hackathonRepository.getHackathonsByStatus(Status.IN_CORSO)) {
+            if(!h.getMentors().contains(hr.getTo())) return null;
+            if(!h.getParticipants().contains(hr.getFrom().getTeam())) return null;
+        }
+        helpRequestRepository.create(hr);
+        notificationService.send("Richiesta di aiuto!",
+                "Hai ricevuto una richiesta di aiuto da " + hr.getFrom().getName(),
+                hr.getTo().getId());
+        return helpRequestRepository.getById(hr.getId());
     }
 
     public void completeHelpRequest(HelpRequestRequester hrr) {
