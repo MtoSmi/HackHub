a3930fdf9aef87c258a3c601daa573a5002b9e91
Redcoral-17 <filippo.corallini@studenti.unicam.it>
2026-03-10 10:08:45 +0100

    Fix validation logic in SubmissionValidator to correctly check for empty title

--- project/app/src/main/java/it/unicam/cs/ids/hackhub/validator/SubmissionValidator.java
+++ project/app/src/main/java/it/unicam/cs/ids/hackhub/validator/SubmissionValidator.java
@@ -9,7 +9,7 @@ public class SubmissionValidator implements Validator<Submission> {
     @Override
     public boolean validate(Submission s) {
         if(s == null) return false;
-        if(s.getTitle() == null || s.getTitle().isEmpty) return false;
+        if(s.getTitle() == null || s.getTitle().isEmpty()) return false;
         if(s.getDescription() == null || s.getDescription().isEmpty()) return false;
         if(s.getStartDate() == null || s.getEndDate() == null) return false;
         if(LocalDateTime.now().isAfter(s.getStartDate())) return false;
