2bdd11fb439dddd579d59f15c84e2c05fd2607b2
MtoSmi <matteo.simonetti@studenti.unicam.it>
2026-03-10 09:30:22 +0100

    Add responses management to Submission class

--- project/app/src/main/java/it/unicam/cs/ids/hackhub/entity/model/Submission.java
+++ project/app/src/main/java/it/unicam/cs/ids/hackhub/entity/model/Submission.java
@@ -1,6 +1,7 @@
 package it.unicam.cs.ids.hackhub.entity.model;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class Submission {
     private long id;
@@ -10,4 +11,63 @@ public class Submission {
     private LocalDateTime endDate;
     private List<Response> responses;
     private boolean complete;
+
+    public Submission() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
+
+    public List<Response> getResponses() {
+        return responses;
+    }
+
+    public void setResponses(List<Response> responses) {
+        this.responses = responses;
+    }
+
+    public boolean isComplete() {
+        return complete;
+    }
+
+    public void setComplete(boolean complete) {
+        this.complete = complete;
+    }
 }
