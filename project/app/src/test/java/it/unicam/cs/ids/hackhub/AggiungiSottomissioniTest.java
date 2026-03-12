be0f223830c2a0ce15ba11b7bf71ea5c17ad4129
MtoSmi <matteo.simonetti@studenti.unicam.it>
2026-03-12 11:14:49 +0100

    Fix typo in SubmissionInterfaceController import and update AggiungiSottomissioniTest to validate multiple submissions

--- project/app/src/test/java/it/unicam/cs/ids/hackhub/AggiungiSottomissioniTest.java
+++ project/app/src/test/java/it/unicam/cs/ids/hackhub/AggiungiSottomissioniTest.java
@@ -1,8 +1,7 @@
 package it.unicam.cs.ids.hackhub;
 
 import it.unicam.cs.ids.hackhub.controller.HackathonInterfaceController;
-import it.unicam.cs.ids.hackhub.controller.MentorInterfaceController;
-import it.unicam.cs.ids.hackhub.controller.SubmissionIterfaceController;
+import it.unicam.cs.ids.hackhub.controller.SubmissionInterfaceController;
 import it.unicam.cs.ids.hackhub.entity.enumeration.Rank;
 import it.unicam.cs.ids.hackhub.entity.model.Hackathon;
 import it.unicam.cs.ids.hackhub.entity.model.Submission;
@@ -13,11 +12,9 @@ import it.unicam.cs.ids.hackhub.repository.HackathonRepository;
 import it.unicam.cs.ids.hackhub.repository.NotificationRepository;
 import it.unicam.cs.ids.hackhub.repository.UserRepository;
 import it.unicam.cs.ids.hackhub.service.HackathonService;
-import it.unicam.cs.ids.hackhub.service.MentorService;
 import it.unicam.cs.ids.hackhub.service.NotificationService;
 import it.unicam.cs.ids.hackhub.service.SubmissionService;
 import it.unicam.cs.ids.hackhub.validator.HackathonValidator;
-import it.unicam.cs.ids.hackhub.validator.MentorValidator;
 import it.unicam.cs.ids.hackhub.validator.SubmissionValidator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -25,7 +22,6 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
-import java.util.List;
 
 public class AggiungiSottomissioniTest {
     private final User user = new User("Mario", "Rossi", "mario.rossi@aol.me", "Password1234");
@@ -34,14 +30,14 @@ public class AggiungiSottomissioniTest {
     private final User mentor1 = new User("Giovanni", "Bianchi", "giovanni.bianchi@tiscali.it", "Password1234");
     private final User mentor2 = new User("Francesca", "Neri", "francesca.neri@infostrada.it", "Password1234");
     private HackathonInterfaceController HackathonController;
-    private SubmissionIterfaceController SubmissionController;
+    private SubmissionInterfaceController SubmissionController;
 
     @BeforeEach
     void setUp() {
         HackathonValidator validator = new HackathonValidator();
         HackathonRepository HackathonRepository = new HackathonRepository();
         HackathonController = new HackathonInterfaceController(new HackathonService(HackathonRepository, validator, new NotificationService(new NotificationRepository(), new UserRepository())));
-        SubmissionController = new SubmissionIterfaceController(new SubmissionService(new SubmissionValidator(), HackathonRepository));
+        SubmissionController = new SubmissionInterfaceController(new SubmissionService(new SubmissionValidator(), HackathonRepository));
         user.setId(1L);
         user.setRank(Rank.STANDARD);
         host.setId(2L);
@@ -63,12 +59,40 @@ public class AggiungiSottomissioniTest {
 
         SubmissionRequester submissionRequester = createValidSubmission();
         Submission submissionResult = SubmissionController.creationSubmission(submissionRequester, 1L);
-        Assertions.assertNotNull(submissionResult, "La sottomissione creata non dovrebbe essere null");
-        Assertions.assertEquals(submissionRequester.getTitle(), submissionResult.getTitle(), "Il titolo della sottomissione creata dovrebbe corrispondere a quello della richiesta");
-        Assertions.assertEquals(submissionRequester.getDescription(), submissionResult.getDescription(), "La descrizione della sottomissione creata dovrebbe corrispondere a quella della richiesta");
-        Assertions.assertEquals(submissionRequester.getStartDate(), submissionResult.getStartDate(), "La data di inizio della sottomissione creata dovrebbe corrispondere a quella della richiesta");
-        Assertions.assertEquals(submissionRequester.getEndDate(), submissionResult.getEndDate(), "La data di fine della sottomissione creata dovrebbe corrispondere a quella della richiesta");
-    }
+
+        Hackathon newResponse  = HackathonController.showSelectedHackathon(1L);
+        Assertions.assertNotNull(newResponse, "L'hackathon dovrebbe essere presente dopo la creazione");
+        Assertions.assertNotNull(newResponse.getSubmissions(), "La lista delle sottomissioni non dovrebbe essere null");
+        Assertions.assertEquals(1, newResponse.getSubmissions().size(), "La lista delle sottomissioni dovrebbe contenere una sottomissione");
+        Assertions.assertEquals(submissionRequester.getTitle(), newResponse.getSubmissions().getFirst().getTitle(), "Il titolo della sottomissione dovrebbe corrispondere a quello della richiesta");
+        Assertions.assertEquals(submissionRequester.getDescription(), newResponse.getSubmissions().getFirst().getDescription(), "La descrizione della sottomissione dovrebbe corrispondere a quella della richiesta");
+
+        }
+
+
+        @Test
+        public void testAggiungiPiuSottomissioni() {
+            HackathonRequester requester = createValidHackathon();
+            Hackathon response = HackathonController.creationHackathon(requester);
+            Assertions.assertNotNull(response);
+            Assertions.assertEquals(1L, response.getId(), "L'ID dell'hackathon creato dovrebbe essere 1");
+
+            SubmissionRequester submissionRequester = createValidSubmission();
+            Submission submissionResult = SubmissionController.creationSubmission(submissionRequester, 1L);
+            SubmissionRequester submissionRequester2 = createValidSubmission();
+            submissionRequester2.setTitle("Sottomissione2");
+            Submission submissionResult2 = SubmissionController.creationSubmission(submissionRequester2, 1L);
+
+            Hackathon newResponse  = HackathonController.showSelectedHackathon(1L);
+            Assertions.assertNotNull(newResponse, "L'hackathon dovrebbe essere presente dopo la creazione");
+            Assertions.assertNotNull(newResponse.getSubmissions(), "La lista delle sottomissioni non dovrebbe essere null");
+            Assertions.assertEquals(2, newResponse.getSubmissions().size(), "La lista delle sottomissioni dovrebbe contenere una sottomissione");
+            Assertions.assertEquals(submissionRequester.getTitle(), newResponse.getSubmissions().getFirst().getTitle(), "Il titolo della sottomissione dovrebbe corrispondere a quello della richiesta");
+            Assertions.assertEquals(submissionRequester.getDescription(), newResponse.getSubmissions().getFirst().getDescription(), "La descrizione della sottomissione dovrebbe corrispondere a quella della richiesta");
+            Assertions.assertEquals("Sottomissione2", newResponse.getSubmissions().getLast().getTitle(), "Il titolo della seconda sottomissione dovrebbe corrispondere a quello della richiesta");
+
+
+        }
 
     private HackathonRequester createValidHackathon() {
         HackathonRequester request = new HackathonRequester();
