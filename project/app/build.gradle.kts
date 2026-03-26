1d4f2b58d36e7db959f434fe01eb9e4e2aa030eb
MtoSmi <matteo.simonetti@studenti.unicam.it>
2026-03-26 17:38:46 +0100

    Add Spring Data JPA and H2 database dependencies

--- project/app/build.gradle.kts
+++ project/app/build.gradle.kts
@@ -28,6 +28,8 @@ dependencies {
     // This dependency is used by the application.
     implementation(libs.guava)
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
