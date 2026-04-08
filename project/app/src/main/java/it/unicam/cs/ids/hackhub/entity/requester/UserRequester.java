f56a2b6ca552cc92b2451737b44a2c42e6f1ac8c
MtoSmi <matteo.simonetti@studenti.unicam.it>
2026-04-08 18:22:23 +0200

    Refactor UserRequester and UserUpdateRequester to use records for improved data handling

--- project/app/src/main/java/it/unicam/cs/ids/hackhub/entity/requester/UserRequester.java
+++ project/app/src/main/java/it/unicam/cs/ids/hackhub/entity/requester/UserRequester.java
@@ -1,12 +1,9 @@
 package it.unicam.cs.ids.hackhub.entity.requester;
 
-import it.unicam.cs.ids.hackhub.entity.model.User;
-
-/**
- * Rappresenta un utente in fase di registrazione.
- * </p>
- * Questa classe estende {@link User} e serve come specializzazione
- * per distinguere gli utenti in fase di input o workflow durante la creazione.
- */
-public class UserRequester extends User {
+public record UserRequester(
+        String name,
+        String surname,
+        String email,
+        String password
+) {
 }
--- project/app/src/main/java/it/unicam/cs/ids/hackhub/entity/requester/UserUpdateRequester.java
+++ project/app/src/main/java/it/unicam/cs/ids/hackhub/entity/requester/UserUpdateRequester.java
@@ -1,11 +1,10 @@
 package it.unicam.cs.ids.hackhub.entity.requester;
 
-import it.unicam.cs.ids.hackhub.entity.model.User;
-
-public class UserUpdateRequester extends User {
-    private Long id;
-    private String name;
-    private String surname;
-    private String email;
-    private String password;
+public record UserUpdateRequester(
+        Long id,
+        String name,
+        String surname,
+        String email,
+        String password
+) {
 }
