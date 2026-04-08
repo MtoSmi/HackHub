8e5af8faddcda908eb0e9bb078f30f4a02a78bd2
MtoSmi <matteo.simonetti@studenti.unicam.it>
2026-04-08 14:07:57 +0200

    Refactor HelpRequestRequester to use records for improved data handling

--- project/app/src/main/java/it/unicam/cs/ids/hackhub/entity/requester/HelpRequestRequester.java
+++ project/app/src/main/java/it/unicam/cs/ids/hackhub/entity/requester/HelpRequestRequester.java
@@ -1,12 +1,9 @@
 package it.unicam.cs.ids.hackhub.entity.requester;
 
-import it.unicam.cs.ids.hackhub.entity.model.HelpRequest;
-
-/**
- * Rappresenta una richiesta di aiuto.
- * </p>
- * Questa classe estende {@link HelpRequest} e serve come specializzazione
- * per distinguere le richieste in fase di input o workflow.
- */
-public class HelpRequestRequester extends HelpRequest {
+public record HelpRequestRequester(
+        String title,
+        String description,
+        Long fromId,
+        Long toId
+) {
 }
